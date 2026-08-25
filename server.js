997e1d46043b842154767c0641021dae1d1bc5b6
dcstechnosismanish <dcstechnosismanish@gmail.com>
2026-08-25 05:00:29 +0000

    Initial CI/CD project

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+        message: "Hello from CI/CD Pipeline 🚀",
+        version: "v1"
+    }));
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
