67d601284fb57d89e9ceaa0314d5da3f578dcab6
dcstechnosismanish <dcstechnosismanish@gmail.com>
2026-08-25 08:57:57 +0000

    Add health and info endpoints

--- server.js
+++ server.js
@@ -1,16 +1,57 @@
 const http = require("http");
 
 const PORT = 3000;
+const VERSION = "v3";
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "application/json"
-    });
+    res.setHeader("Content-Type", "application/json");
 
-    res.end(JSON.stringify({
-        message: "Hello from CI/CD Pipeline 🚀",
-        version: "v2"
-    }));
+    // Main application endpoint
+    if (req.url === "/cicd/" || req.url === "/") {
+        res.writeHead(200);
+
+        return res.end(
+            JSON.stringify({
+                message: "Hello from CI/CD Pipeline 🚀",
+                version: VERSION
+            })
+        );
+    }
+
+    // Health check endpoint
+    if (req.url === "/health") {
+        res.writeHead(200);
+
+        return res.end(
+            JSON.stringify({
+                status: "healthy",
+                version: VERSION
+            })
+        );
+    }
+
+    // Application information
+    if (req.url === "/info") {
+        res.writeHead(200);
+
+        return res.end(
+            JSON.stringify({
+                application: "CI/CD Real Project",
+                version: VERSION,
+                environment: "production",
+                platform: "Docker"
+            })
+        );
+    }
+
+    // 404
+    res.writeHead(404);
+
+    res.end(
+        JSON.stringify({
+            error: "Route not found"
+        })
+    );
 });
 
 server.listen(PORT, "0.0.0.0", () => {
